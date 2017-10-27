4ff4d2efd130aa16076ce4334a5a9ab9404d3326
simon.rove.jensen <simon.rove.jensen@gmail.com>
2017-10-27 13:30:01 +0200

    added getters and setters

--- Subproject 2/Subproject 2/Post.cs
+++ Subproject 2/Subproject 2/Post.cs
@@ -6,5 +6,26 @@ namespace Subproject_2
 {
     class Post
     {
+        public int id { get; set; }
+
+        public int type { get; set; }
+
+        public int parent_id { get; set; }
+
+        public int answer_id { get; set; }
+
+        public DateTime creationDate { get; set; }
+
+        public int score { get; set; }
+
+        public string text { get; set; }
+
+        public DateTime closedDate { get; set; }
+
+        public string title { get; set; }
+
+        public User user { get; set; }
+
+        public List<Tag> tags { get; set; }
     }
 }
--- Subproject 2/Subproject 2/User.cs
+++ Subproject 2/Subproject 2/User.cs
@@ -6,5 +6,19 @@ namespace Subproject_2
 {
     class User
     {
+
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+        public DateTime creationDate { get; set; }
+
+        public string location { get; set; }
+
+        public int age { get; set; }
+
+        public List<Marking> marks { get; set; }
+
+        public List<History> history { get; set; }
     }
 }
