a4bea97cb6dad5bda72208fccdca1bbc2d0bac7d
simon.rove.jensen <simon.rove.jensen@gmail.com>
2017-10-27 13:02:20 +0200

    Created subproject 2

---
+++ Subproject 2/Subproject 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Subproject_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
