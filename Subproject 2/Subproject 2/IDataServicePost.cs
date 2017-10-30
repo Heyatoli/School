3bfe5775d510c58396f751b2289ddaa73713694a
simon.rove.jensen <simon.rove.jensen@gmail.com>
2017-10-30 14:09:15 +0100

    defined the interfaces

--- Subproject 2/Subproject 2/IDataServicePost.cs
+++ Subproject 2/Subproject 2/IDataServicePost.cs
@@ -6,5 +6,18 @@ namespace Subproject_2
 {
     interface IDataServicePost
     {
-    }
+        List<Post> getPost();
+
+
+        List<Post> getPostWord(string postword);
+            //search post title by given name 
+
+        List<Post> getPostByTag(string tag);
+            //show posts by tag
+
+        List<Post> getPostByUser(int postuserid);
+            //list posts by user
+
+        Post getCommments(int postid);
+    }       
 }
--- Subproject 2/Subproject 2/IDataServiceUser.cs
+++ Subproject 2/Subproject 2/IDataServiceUser.cs
@@ -6,5 +6,25 @@ namespace Subproject_2
 {
     interface IDataServiceUser
     {
+        List<User> getUser();
+
+        List<User> getUsername(string u);
+            //search for user, both whole name, and input contains part of name    
+        
+        List<History> getHistory(int userid); 
+            //show user history 
+        
+        List<Marking> getFavourites(int userid);
+            //show user marking 
+        
+        History createHistory(int userid, string search);
+
+        Marking createMarking(int userid, int postid, string note);
+
+        Marking updateMarking(int userid, int postid, string note);
+
+        bool deleteHistory(int histId);
+
+        bool deleteFavourites(int markId);
     }
 }
