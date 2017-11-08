c5dc7c6a45b87d999682d808e91aac7b3e36463c
simon.rove.jensen <simon.rove.jensen@gmail.com>
2017-11-08 13:04:37 +0100

    created is done, for both the user and the markings

---
+++ subproject 2/stackoverflowwebservice/controller/usercontroller.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Subproject_2;        //Our own dataservice as a library
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+
+namespace StackoverflowWebservice.Controllers
+{
+    [Route("/api/users")]
+    public class UserController : Controller
+    {
+
+        private const int standardPageSize = 15;
+
+        private IDataServiceUser _dataService;
+
+        public UserController(IDataServiceUser dataService)
+        {
+            _dataService = dataService;
+        }
+
+        [HttpGet(Name = nameof(GetUsers))]
+        public IActionResult GetUsers(int page = 0, int pageSize = standardPageSize)
+        {
+            
+            var users = _dataService.getUser(page, pageSize);
+            var totalUsers = users.Count();
+            var totalPages = GetTotalPages(pageSize, totalUsers);
+            if (users == null) return NotFound();
+            var result = new
+            {
+                Total = totalUsers,
+                Pages = totalPages,
+                Page = page,
+                Prev = Link(nameof(GetUsers), page, pageSize, -1, () => page > 0),
+                Next = Link(nameof(GetUsers), page, pageSize, 1, () => page < totalPages - 1),
+                Url = Link(nameof(GetUsers), page, pageSize),
+                Data = users
+            };
+            return Ok(result);
+        }
+
+        [HttpGet("{name}", Name = nameof(GetUsersByName))]
+        public IActionResult GetUsersByName(string name, int page = 0, int pageSize = standardPageSize)
+        {
+            var users = _dataService.getUsername(name, page, pageSize);
+            var totalUsers = users.Count();
+            var totalPages = GetTotalPages(pageSize, totalUsers);
+            if (users == null) return NotFound();
+            var result = new
+            {
+                Total = totalUsers,
+                Pages = totalPages,
+                Page = page,
+                Prev = Link(nameof(GetUsers), page, pageSize, -1, () => page > 0),
+                Next = Link(nameof(GetUsers), page, pageSize, 1, () => page < totalPages - 1),
+                Url = Link(nameof(GetUsers), page, pageSize),
+                Data = users
+            };
+            return Ok(result);
+        }
+
+        [HttpGet, Route("history/{userId}", Name = nameof(GetUserHistory))]
+        public IActionResult GetUserHistory(int userId, int page = 0, int pageSize = standardPageSize)
+        {
+            var history = _dataService.getHistory(userId, page, pageSize);
+            var totalUsers = history.Count();
+            var totalPages = GetTotalPages(pageSize, totalUsers);
+            if (history == null) return NotFound();
+            var result = new
+            {
+                Total = totalUsers,
+                Pages = totalPages,
+                Page = page,
+                Prev = Link(nameof(GetUsers), page, pageSize, -1, () => page > 0),
+                Next = Link(nameof(GetUsers), page, pageSize, 1, () => page < totalPages - 1),
+                Url = Link(nameof(GetUsers), page, pageSize),
+                Data = history
+            };
+            return Ok(history);
+        }
+
+        [HttpGet("markings/{userId}", Name = nameof(GetUserMarkings))]
+        public IActionResult GetUserMarkings(int userId, int page = 0, int pageSize = standardPageSize)
+        {
+            var markings = _dataService.getFavourites(userId, page, pageSize);
+            if (markings == null) return NotFound();
+            return Ok(JsonConvert.SerializeObject(markings));
+        }
+
+        [HttpDelete("history/{histId}")]
+        public IActionResult DeleteUserHistory(int histId)
+        {
+            var delete = _dataService.deleteHistory(histId);
+            if (delete == false) return NotFound();
+            return Ok(histId);
+
+        }
+
+        [HttpDelete("markings/{postId}/{userId}")]
+        public IActionResult DeleteUserMarking(int postId, int userId)
+        {
+            var delete = _dataService.deleteFavourites(postId, userId);
+            if (delete == false) return NotFound();
+            return Ok(postId);
+
+        }
+
+        [HttpPost("history", Name =nameof(PostUserHistory))]
+        public IActionResult PostUserHistory([FromBody] History value)
+        {
+            var history = _dataService.createHistory(value.userId, value.searchWord);
+            var url = Url.Link(nameof(GetUserHistory), new { history.userId });
+            var result = new
+            {
+                Search = history.searchWord,
+                User = url.ToString(),
+            };
+            return Created(url, result);
+            //return Json(JsonConvert.SerializeObject(value));
+        }
+
+        [HttpPost("markings", Name =nameof(PostUserMarking))]
+        public IActionResult PostUserMarking([FromBody] Marking value)
+        {
+            var marking = _dataService.createMarking(value.userID, value.postId, value.note);
+            var url = Url.Link(nameof(GetUserMarkings), new { marking.userID});
+            var result = new
+            {
+                Post = "Bla bla",
+                User = url.ToString(),
+                Note = marking.note
+            };
+            return Created(url, result);
+            //return Json(JsonConvert.SerializeObject(value));
+        }
+
+        [HttpPut, Route("markings")]
+        public IActionResult PutMarkings([FromBody] Marking value)
+        {
+            var update = _dataService.updateMarking(value.userID, value.postId, value.note);
+            if (update == false) return NotFound();
+            return Ok();
+        }
+
+        private static int GetTotalPages(int pageSize, int total)
+        {
+            return (int)Math.Ceiling(total / (double)pageSize);
+        }
+
+        private string Link(string route, int page, int pageSize, int pageInc = 0, Func<bool> f = null)
+        {
+            if (f == null) return Url.Link(route, new { page, pageSize });
+
+            return f()
+                ? Url.Link(route, new { page = page + pageInc, pageSize })
+                : null;
+        }
+    }
+}
